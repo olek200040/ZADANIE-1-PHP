1e35124222ff8665261299d72c7e644ccf32ab43
olek200040 <grymula.oliwier@zst.radom.pl>
2026-09-21 13:06:32 +0200

    Add files via upload

---
+++ Zadanie1PHP.php
@@ -0,0 +1,28 @@
+<?php
+$a = 10; 
+
+echo "1. Wartość zmiennej \$a wynosi: " . $a . "<br>";
+
+$przeciwna = -$a;
+echo "2. Liczba przeciwna do \$a to: " . $przeciwna . "<br>";
+
+$powiekszona = $a + 3;
+echo "3. Zmienna \$a powiększona o 3 wynosi: " . $powiekszona . "<br>";
+
+$pomniejszona = $a - 5;
+echo "4. Zmienna \$a pomniejszona o 5 wynosi: " . $pomniejszona . "<br>";
+
+$pomnozona = $a * 2;
+echo "5. Zmienna \$a pomnożona przez 2 wynosi: " . $pomnozona . "<br>";
+
+$podzielona = $a / 5;
+echo "6. Zmienna \$a podzielona przez 5 wynosi: " . $podzielona . "<br>";
+
+$reszta = $a % 2;
+echo "7. Reszta z dzielenia zmiennej \$a przez 2 wynosi: " . $reszta . "<br>";
+
+$potega = pow($a, 3);
+echo "8. Zmienna \$a podniesiona do 3 potęgi wynosi: " . $potega . "<br>";
+
+echo "9. Wartość zmiennej \$a wynosi: " . $a . " i już się nie zmieni.<br>";
+?>
